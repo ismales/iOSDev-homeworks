deadaa18a98a60a7a5da53431ae19d88b0fe6a1f
Ismail Aleskerov <manofsteel1992@gmail.com>
2023-02-02 11:13:02 +0300

    in the postVC moved the info button to the navbar

--- Navigation/Feed/PostViewController.swift
+++ Navigation/Feed/PostViewController.swift
@@ -15,35 +15,24 @@ final class PostViewController: UIViewController {
         self.textForLabel = textForLabel
         super.init(nibName: nil, bundle: nil)
     }
-
-    private let infoPostButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 30, y: 550, width: 120, height: 50))
-        button.setTitle("ИНФО", for: .normal)
-        button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        return button
-    }()
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupPostButton() {
-        view.addSubview(infoPostButton)
-        infoPostButton.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.3245105861, green: 0.326969422, blue: 0.3208761471, alpha: 1)
+        view.backgroundColor = .gray
         title = textForLabel.title
-        setupPostButton()
+
+        let navBarButton = UIBarButtonItem(title: "Инфо", style: .plain, target: self, action: #selector(tapAction))
+        navigationItem.rightBarButtonItem = navBarButton
     }
 
     @objc private func tapAction() {
         let infoVC = InfoViewController()
         present(infoVC, animated: true)
     }
-
 }
 
 struct Post {
--- Navigation/TabBarViewController.swift
+++ Navigation/TabBarViewController.swift
@@ -8,24 +8,24 @@
 import UIKit
 
 final class TabBarViewController: UITabBarController {
-
+    
     private let feedVC = FeedViewController()
     private let profileVC = ProfileViewController()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupConrollers()
     }
-
+    
     private func setupConrollers() {
         let feedNavigationController = UINavigationController(rootViewController: feedVC)
         feedVC.tabBarItem.title = "Лента"
         feedVC.tabBarItem.image = UIImage(systemName: "newspaper")
-
+        
         let profileNavigationController = UINavigationController(rootViewController: profileVC)
         profileVC.tabBarItem.title = "Профиль"
         profileVC.tabBarItem.image = UIImage(systemName: "person")
-
+        
         viewControllers = [feedNavigationController, profileNavigationController]
     }
 }
